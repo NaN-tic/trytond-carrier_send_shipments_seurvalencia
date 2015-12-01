43584a3419948dcefefbe3dd6d788dcadcfcef7a
resteve <resteve@zikzakmedia.com>
2015-12-01 11:05:17 +0100

    Tests

--- tests/test_carrier_send_shipments_seurvalencia.py
+++ tests/test_carrier_send_shipments_seurvalencia.py
@@ -1,32 +1,18 @@
-#!/usr/bin/env python
 # This file is part of the carrier_send_shipments_seurvalencia module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierSendShipmentsSeurValenciaTestCase(unittest.TestCase):
-    'Test Carrier Send Shipments Seur module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('carrier_send_shipments_seurvalencia')
-
-    def test0005views(self):
-        'Test views'
-        test_view('carrier_send_shipments_seurvalencia')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class CarrierSendShipmentsSeurvalenciaTestCase(ModuleTestCase):
+    'Test Carrier Send Shipments Seurvalencia module'
+    module = 'carrier_send_shipments_seurvalencia'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        CarrierSendShipmentsSeurValenciaTestCase))
+        CarrierSendShipmentsSeurvalenciaTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
